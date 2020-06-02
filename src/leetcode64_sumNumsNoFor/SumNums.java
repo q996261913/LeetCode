ac2252c80e4018edf272c6de42e900076480d317
wyq <99626193@qq.com>
2020-06-02 22:21:59 +0800

    leetcode64不用for循环的两种求和

---
+++ src/leetcode64_sumNumsNoFor/SumNums.java
@@ -0,0 +1,23 @@
+package leetcode64_sumNumsNoFor;
+
+public class SumNums {
+    /**
+     * 用递归求和
+     * @param n
+     * @return
+     */
+    public int sumNum1(int n){
+        return  n*1+n*(n-1)/2;
+    }
+
+    /**
+     * 用递归
+     * @param n
+     * @return
+     */
+    public  int sumNum2(int n){
+        int sum=n;
+        boolean flag=n>0&&(sum+=sumNum2(n-1))>0;
+        return  sum;
+    }
+}
